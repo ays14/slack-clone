b9b85741f50fa63af4fc973c6442e8e2e2a98bdd
ays14 <ayush.sh143@gmail.com>
2019-05-21 12:11:00 +0530

    add restricted route - create team

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -1,62 +1,55 @@
 import React from 'react';
-import { Form, Message, Button, Input, Container, Header } from 'semantic-ui-react';
+import { extendObservable } from 'mobx';
+import { observer } from 'mobx-react';
+import { Message, Form, Button, Input, Container, Header } from 'semantic-ui-react';
 import { gql, graphql } from 'react-apollo';
 
-class Register extends React.Component {
-  state = {
-    username: '',
-    usernameError: '',
-    email: '',
-    emailError: '',
-    password: '',
-    passwordError: '',
-  };
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    extendObservable(this, {
+      email: '',
+      password: '',
+      errors: {},
+    });
+  }
 
   onSubmit = async () => {
-    this.setState({
-      usernameError: '',
-      emailError: '',
-      passwordError: '',
-    });
-
-    const { username, email, password } = this.state;
+    const { email, password } = this;
     const response = await this.props.mutate({
-      variables: { username, email, password },
+      variables: { email, password },
     });
 
-    const { ok, errors } = response.data.register;
+    console.log(response);
+
+    const {
+      ok, token, refreshToken, errors,
+    } = response.data.login;
 
     if (ok) {
+      localStorage.setItem('token', token);
+      localStorage.setItem('refreshToken', refreshToken);
       this.props.history.push('/');
     } else {
       const err = {};
       errors.forEach(({ path, message }) => {
-        // err['passwordError'] = 'too long..';
         err[`${path}Error`] = message;
       });
 
-      this.setState(err);
+      this.errors = err;
     }
-
-    console.log(response);
   };
 
   onChange = (e) => {
     const { name, value } = e.target;
-    // name = "email";
-    this.setState({ [name]: value });
+    this[name] = value;
   };
 
   render() {
-    const {
-      username, email, password, usernameError, emailError, passwordError,
-    } = this.state;
+    const { email, password, errors: { emailError, passwordError } } = this;
 
     const errorList = [];
-
-    if (usernameError) {
-      errorList.push(usernameError);
-    }
 
     if (emailError) {
       errorList.push(emailError);
@@ -68,17 +61,8 @@ class Register extends React.Component {
 
     return (
       <Container text>
-        <Header as="h2">Register</Header>
+        <Header as="h2">Login</Header>
         <Form>
-          <Form.Field error={!!usernameError}>
-            <Input
-              name="username"
-              onChange={this.onChange}
-              value={username}
-              placeholder="Username"
-              fluid
-            />
-          </Form.Field>
           <Form.Field error={!!emailError}>
             <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
           </Form.Field>
@@ -102,10 +86,12 @@ class Register extends React.Component {
   }
 }
 
-const registerMutation = gql`
-  mutation($username: String!, $email: String!, $password: String!) {
-    register(username: $username, email: $email, password: $password) {
+const loginMutation = gql`
+  mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       ok
+      token
+      refreshToken
       errors {
         path
         message
@@ -114,4 +100,4 @@ const registerMutation = gql`
   }
 `;
 
-export default graphql(registerMutation)(Register);
+export default graphql(loginMutation)(observer(Login));
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 import Home from './Home';
 import Register from './Register';
+import Login from './Login';
+import CreateTeam from './CreateTeam';
+
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  try {
+    jwtDecode(token);
+    jwtDecode(refreshToken);
+  } catch (err) {
+    return false;
+  }
+
+  return true;
+};
+
+/* eslint-disable */ 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      (isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/login',
+          }}
+        />
+      ))}
+  />
+);
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
+      <Route path="/login" exact component={Login} />
+      <PrivateRoute path="/create-team" exact component={CreateTeam} />
     </Switch>
   </BrowserRouter>
 );
+
